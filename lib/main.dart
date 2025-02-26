ec573b4e9d04162fcbb4c408b79dad5ea7acb461
nikhilgoud003 <nikhilgoud2001@gmail.com>
2025-02-26 18:39:33 -0500

    Added Fading text code

--- lib/main.dart
+++ lib/main.dart
@@ -30,3 +30,52 @@ class FadingTextAnimation extends StatefulWidget {
   @override
   _FadingTextAnimationState createState() => _FadingTextAnimationState();
 }
+
+class _FadingTextAnimationState extends State<FadingTextAnimation> {
+  bool _isVisible = true;
+  bool _isDarkMode = false;
+  bool _showFrame = false;
+  Color _textColor = Colors.black;
+  final PageController _pageController = PageController();
+
+  void toggleVisibility() {
+    setState(() {
+      _isVisible = !_isVisible;
+    });
+  }
+
+  void toggleTheme() {
+    setState(() {
+      _isDarkMode = !_isDarkMode;
+      _textColor = _isDarkMode ? Colors.white : Colors.black;
+    });
+  }
+
+  void showColorPicker() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Pick a color'),
+          content: SingleChildScrollView(
+            child: ColorPicker(
+              onColorChanged: (Color color) {
+                setState(() {
+                  _textColor = color;
+                });
+              },
+              pickerColor: _textColor,
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Done'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
